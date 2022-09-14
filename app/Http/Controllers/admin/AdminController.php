3691aeb146f090e1a4a7480162c026f8ac62e871
Akmal Khan <akmalkhancreativetechsol@gmail.com>
2022-09-14 20:15:40 +0500

    Dashboard Charts Completed

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -6,6 +6,7 @@ use App\Models\Employee;
 use App\Models\Department;
 use App\Models\Attendance;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
@@ -14,11 +15,42 @@ class AdminController extends Controller
     {
       $data['title'] = 'Dashboard';
       $data['content_header'] = 'Dashboard';
+      $departments = Department::all();
       $data['totalEmployees'] = count(Employee::all());
-      $data['totalDepartments'] = count(Department::all());
-      // $data['totalAtendee'] = count(Department::all());
-      
+      $data['totalDepartments'] = count($departments);
+      $data['totalAttendee'] = Attendance::where('date', Carbon::now()->format('d-m-Y'))->count();
+      $data['totalKeyAssigned'] = Attendance::where('date', Carbon::now()->format('d-m-Y'))
+          ->join('employees as e', function ($eJoin){
+              $eJoin->on('attendances.employee_id', '=', 'e.id')
+                  ->where('e.key_authority', 1);
+          })->count();
+      $departmentNamesForBarChart = $totalDepartmentPresentEmployees =
+      $totalDepartmentTimedOutEmployees = [];
+      foreach ($departments as $department){
+          $departmentNamesForBarChart[] = $department->name;
+          $totalDepartmentPresentEmployees[] = Attendance::where('date', Carbon::now()->format('d-m-Y'))
+              ->whereNull('attendances.time_out')
+              ->join('employees as e', function ($eJoin) use ($department){
+                  $eJoin->on('attendances.employee_id', '=', 'e.id')
+                      ->join('departments as d', function ($dJoin) use ($department){
+                          $dJoin->on('e.department_id', '=', 'd.id')
+                              ->where('d.id', $department->id);
+                      });
+              })->count();
+          $totalDepartmentTimedOutEmployees[] = Attendance::where('date', Carbon::now()->format('d-m-Y'))
+              ->whereNotNull('attendances.time_out')
+              ->join('employees as e', function ($eJoin) use ($department){
+                  $eJoin->on('attendances.employee_id', '=', 'e.id')
+                      ->join('departments as d', function ($dJoin) use ($department){
+                          $dJoin->on('e.department_id', '=', 'd.id')
+                              ->where('d.id', $department->id);
+                      });
+              })->count();
+      }
+      $data['totalDepartmentPresentEmployees'] = $totalDepartmentPresentEmployees;
+      $data['departmentNamesForBarChart'] = $departmentNamesForBarChart;
+      $data['totalDepartmentTimedOutEmployees'] = $totalDepartmentTimedOutEmployees;
       return view('admin.dashboard', $data);
     }
-   
+
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,73 +13,113 @@
 @section('content')
     <!-- <p>Welcome to Attendance With Face Admin Dashbord</p> -->
     <div class="row">
-      
         <div class="col-md-3">
-    {{-- Themes --}}
-    <x-adminlte-small-box title="{{ $totalEmployees }}" text="Total Employees" icon="fas fa-eye text-dark"
-        theme="teal" url="#" url-text="View details"/>
+            <x-adminlte-small-box title="{{ $totalEmployees }}" text="Total Employees" icon="fas fa-eye text-dark"
+                                  theme="teal" url="{{ route('employee.index') }}" url-text="View details"/>
+        </div>
+        <div class="col-md-3 ">
+            <x-adminlte-small-box title="{{ $totalDepartments }}" text="Total Departments" icon="fas fa-eye text-dark"
+                                  theme="teal" url="{{ route('department.index') }}" url-text="View details"/>
+        </div>
+        <div class="col-md-3 ">
+            <x-adminlte-small-box title="{{ $totalAttendee }}" text="Total Attendee" icon="fas fa-eye text-dark"
+                                  theme="teal" url="{{ route('attendance.index') }}" url-text="View details"/>
+        </div>
+        <div class="col-md-3">
+            <x-adminlte-small-box title="{{ $totalKeyAssigned }}" text="Keys Issued" icon="fas fa-key text-dark"
+                                  theme="teal" url="{{ route('assignkey.index') }}" url-text="View details"/>
+        </div>
+        <!-- Bar Chart Start -->
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header border-0">
+                    <div class="d-flex justify-content-between">
+                        <h3 class="card-title">Attendance Department Wise</h3>
+                        <a href="{{ route('attendance.index') }}">View Report</a>
+                    </div>
+                </div>
+                <div class="card-body">
+{{--                    <div class="d-flex">--}}
+{{--                        <p class="d-flex flex-column">--}}
+{{--                            <span class="text-bold text-lg">$18,230.00</span>--}}
+{{--                            <span>Sales Over Time</span>--}}
+{{--                        </p>--}}
+{{--                        <p class="ml-auto d-flex flex-column text-right">--}}
+{{--                    <span class="text-success">--}}
+{{--                      <i class="fas fa-arrow-up"></i> 33.1%--}}
+{{--                    </span>--}}
+{{--                            <span class="text-muted">Since last month</span>--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+                    <!-- /.d-flex -->
 
-   </div>
-   <div class="col-md-3 ">
-    {{-- Themes --}}
-    <x-adminlte-small-box title="{{ $totalDepartments }}" text="Total Departments" icon="fas fa-eye text-dark"
-        theme="teal" url="#" url-text="View details"/>
+                    <div class="position-relative mb-4">
+                        <canvas id="department-stacked-chart" height="400"></canvas>
+                    </div>
 
-   </div>
-   <div class="col-md-3 ">
-    {{-- Themes --}}
-    <x-adminlte-small-box title="{{ $totalAtendee }}" text="Total Attendee" icon="fas fa-eye text-dark"
-        theme="teal" url="#" url-text="View details"/>
+                    <div class="d-flex flex-row justify-content-end">
+                        <span class="mr-2">
+                            <i class="fas fa-square text-success"></i> Present
+                        </span>
+                        <span>
+                            <i class="fas fa-square text-gray"></i> Timed Out
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Donught Chart Start -->
+        <div class="col-6">
+            <div class="card">
+                <div class="card-header border-0">
+                    <div class="d-flex justify-content-between">
+                        <h3 class="card-title">Attendance Department Graphic View</h3>
+{{--                        <a href="{{ route('attendance.index') }}">View Report</a>--}}
+                    </div>
+                </div>
+                <div class="card-body pb-5">
+                {{--                    <div class="d-flex">--}}
+                {{--                        <p class="d-flex flex-column">--}}
+                {{--                            <span class="text-bold text-lg">$18,230.00</span>--}}
+                {{--                            <span>Sales Over Time</span>--}}
+                {{--                        </p>--}}
+                {{--                        <p class="ml-auto d-flex flex-column text-right">--}}
+                {{--                    <span class="text-success">--}}
+                {{--                      <i class="fas fa-arrow-up"></i> 33.1%--}}
+                {{--                    </span>--}}
+                {{--                            <span class="text-muted">Since last month</span>--}}
+                {{--                        </p>--}}
+                {{--                    </div>--}}
+                <!-- /.d-flex -->
+                    <div class="position-relative mb-4">
+                        <canvas id="department-donought-chart" height="400"></canvas>
+                    </div>
 
-   </div>
-   <div class="col-md-3">
-  <x-adminlte-small-box title="10" text="LATE TODAY" icon="fas fa-eye text-dark"
-        theme="teal" url="#" url-text="View details"/>
-
+{{--                    <div class="d-flex flex-row justify-content-end">--}}
+{{--                        <span class="mr-2">--}}
+{{--                            <i class="fas fa-square text-success"></i> Present--}}
+{{--                        </span>--}}
+{{--                        <span>--}}
+{{--                            <i class="fas fa-square text-gray"></i> Timed Out--}}
+{{--                        </span>--}}
+{{--                    </div>--}}
+                </div>
+            </div>
+        </div>
     </div>
-    </div>
-{{-- @push('js')
-<script>
-
-    $(document).ready(function() {
-
-        let sBox = new _AdminLTE_SmallBox('sbUpdatable');
-
-        let updateBox = () =>
-        {
-            // Stop loading animation.
-            sBox.toggleLoading();
-
-            // Update data.
-            let rep = Math.floor(1000 * Math.random());
-            let idx = rep < 100 ? 0 : (rep > 500 ? 2 : 1);
-            let text = 'Reputation - ' + ['Basic', 'Silver', 'Gold'][idx];
-            let icon = 'fas fa-medal ' + ['text-primary', 'text-light', 'text-warning'][idx];
-            let url = ['url1', 'url2', 'url3'][idx];
-
-            let data = {text, title: rep, icon, url};
-            sBox.update(data);
-        };
-
-        let startUpdateProcedure = () =>
-        {
-            // Simulate loading procedure.
-            sBox.toggleLoading();
-
-            // Wait and update the data.
-            setTimeout(updateBox, 2000);
-        };
-
-        setInterval(startUpdateProcedure, 10000);
-    })
-
-</script>
-@endpush --}}
 @stop
+@section('plugins.Chartjs', true)
 @section('css')
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
 @stop
 
 @section('js')
-    {{-- <script> console.log('Hi!'); </script> --}}
+    <script type="text/javascript">
+        var departmentNamesForBarChart = {{ Js::From($departmentNamesForBarChart) }};
+        var totalAttendee = {{ Js::From($totalAttendee) }};
+        var totalDepartmentPresentEmployees = {{ Js::from($totalDepartmentPresentEmployees) }};
+        var totalDepartmentTimedOutEmployees = {{ Js::from($totalDepartmentTimedOutEmployees) }};
+    </script>
+    <!-- Dashboard Script -->
+    <script src="{{ asset('js/dashboard.js') }}"></script>
 @stop
